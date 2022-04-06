b468a52bafce764f66fcce962e996dc44cae2e43
Adam <adam.milko@gmail.com>
2022-04-06 23:35:47 +0200

    added some styling

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,8 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
@@ -10,13 +12,17 @@ import {MatIconModule} from '@angular/material/icon';
     MatTableModule, 
     MatPaginatorModule, 
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule
 ],
   exports: [
       MatTableModule, 
       MatPaginatorModule, 
       MatToolbarModule, 
-      MatIconModule
+      MatIconModule,
+      MatButtonModule,
+      MatMenuModule
     ],
 })
 export class MaterialModule {}
